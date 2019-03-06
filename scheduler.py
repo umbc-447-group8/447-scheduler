41027c979860cbfeaa5658e4f3eb57b8fc2018f0
lucj1 <lucj1@linux3.gl.umbc.edu>
2019-03-06 14:35:50 -0500

    Google algorithm with comments to help start design

---
+++ scheduler.py
@@ -0,0 +1,105 @@
+from __future__ import division
+from __future__ import print_function
+from ortools.sat.python import cp_model
+
+###To start, we should focus on being able to fill quotas for doctors and PAs.
+###We could possibly use the same algo for doctors and PAs, but it might be easier to have two seperate
+###algorithms for each as they have different variable constraints
+
+def main():
+    # This program tries to find an optimal assignment of nurses to shifts
+    # (3 shifts per day, for 7 days), subject to some constraints (see below).
+    # Each nurse can request to be assigned to specific shifts.
+    # The optimal assignment maximizes the number of fulfilled shift requests.
+
+    #These can be user defined variables that will be saved.
+    ###Also include places where the days can be split up, such as 8 hour shifts during weekdays and 12 hour during weekends.
+    ###Could ask for which days have how many x hour shifts and can be calculated after that.
+    ###IMPORTANT: Keep track of the # of weeks in a month and how the weeks are filled. 
+    num_nurses = 5
+    num_shifts = 3
+    num_days = 7
+    all_nurses = range(num_nurses)
+    all_shifts = range(num_shifts)
+    all_days = range(num_days)
+
+    ###This could be expanded to go from 0 - 10, 0 being no request, and 10 being a high priority request/ non-negotiable
+    ##Also user definable
+    shift_requests = [[[0, 0, 1], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 1],
+                       [0, 1, 0], [0, 0, 1]],
+                      [[0, 0, 0], [0, 0, 0], [0, 1, 0], [0, 1, 0], [1, 0, 0],
+                       [0, 0, 0], [0, 0, 1]],
+                      [[0, 1, 0], [0, 1, 0], [0, 0, 0], [1, 0, 0], [0, 0, 0],
+                       [0, 1, 0], [0, 0, 0]],
+                      [[0, 0, 1], [0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 0],
+                       [1, 0, 0], [0, 0, 0]],
+                      [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 0, 0], [1, 0, 0],
+                       [0, 1, 0], [0, 0, 0]]]
+  
+    # Creates the model.
+    model = cp_model.CpModel()
+
+    # Creates shift variables.
+    # shifts[(n, d, s)]: nurse 'n' works shift 's' on day 'd'.
+    ###This also has to be able to be modified to satisfy the # of shifts
+    shifts = {}
+    for n in all_nurses:
+        for d in all_days:
+            for s in all_shifts:
+                shifts[(n, d,
+                        s)] = model.NewBoolVar('shift_n%id%is%i' % (n, d, s))
+
+    # Each shift is assigned to exactly one nurse in .
+    ###This is again a place where we can change the amount of nurses/doctors working during one shift
+    for d in all_days:
+        for s in all_shifts:
+            model.Add(sum(shifts[(n, d, s)] for n in all_nurses) == 1)
+
+    # Each nurse works at most one shift per day.
+    ###Should hold true
+    for n in all_nurses:
+        for d in all_days:
+            model.Add(sum(shifts[(n, d, s)] for s in all_shifts) <= 1)
+
+    # min_shifts_assigned is the largest integer such that every nurse can be
+    # assigned at least that number of shifts.
+    ###Depending on constraints, nurses might not have to work a certain amount of shifts,
+    ###Here would be a good place to instead track if a nurse has made a quoto or not, and ajust the schedule accordingly
+    min_shifts_per_nurse = (num_shifts * num_days) // num_nurses
+    max_shifts_per_nurse = min_shifts_per_nurse + 1
+    for n in all_nurses:
+        num_shifts_worked = sum(
+            shifts[(n, d, s)] for d in all_days for s in all_shifts)
+        model.Add(min_shifts_per_nurse <= num_shifts_worked)
+        model.Add(num_shifts_worked <= max_shifts_per_nurse)
+
+    model.Maximize(
+        sum(shift_requests[n][d][s] * shifts[(n, d, s)] for n in all_nurses
+            for d in all_days for s in all_shifts))
+    
+
+    # Creates the solver and solve.
+    ###If there is a conflict we will have to add in moonlighters.
+    solver = cp_model.CpSolver()
+    solver.Solve(model)
+    for d in all_days:
+        print('Day', d)
+        for n in all_nurses:
+            for s in all_shifts:
+                if solver.Value(shifts[(n, d, s)]) == 1:
+                    if shift_requests[n][d][s] == 1:
+                        print('Nurse', n, 'works shift', s, '(requested).')
+                    else:
+                        print('Nurse', n, 'works shift', s, '(not requested).')
+        print()
+
+    # Statistics.
+    print()
+    print('Statistics')
+    print('  - Number of shift requests met = %i' % solver.ObjectiveValue(),
+          '(out of', num_nurses * min_shifts_per_nurse, ')')
+    print('  - wall time       : %f s' % solver.WallTime())
+
+
+if __name__ == '__main__':
+    main()
