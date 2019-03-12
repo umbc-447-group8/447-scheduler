5e7bac05f4c9c5edf8b3c7c3f4246edd861b7822
Diana Bell <diana.mickey.bell@gmail.com>
2019-03-12 09:34:04 -0400

    some todos for scheduler2

--- scheduler2.py
+++ scheduler2.py
@@ -10,6 +10,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#TODO: separate constraint objects into separate file
+#TODO: Split 
+
 """Creates a shift scheduling problem and solves it."""
 
 from __future__ import print_function
