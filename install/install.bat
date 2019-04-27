417e5db2e4df83f08e37f7f4639eddaa3c87cc80
Diana Bell <47399312+bagillsumbc@users.noreply.github.com>
2019-04-26 22:27:24 -0400

    add other flask dependencies

--- install/install.bat
+++ install/install.bat
@@ -7,8 +7,11 @@ python-3.7.0.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
 python get-pip.py
 
-:: Install Flask
+:: Install Flask Dependencies
 pip install flask
+pip install flask-restful
+pip install flask-cors
 
 :: Install OR-Tools
 python -m pip install --user ortools
+
